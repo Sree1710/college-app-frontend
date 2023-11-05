38d9fcc3df521704a42a436aeb55dafbe7a6c2bd
Sree1710 <sreelekshmisl1710@gmail.com>
2023-11-05 19:22:38 +0530

    StudNavBar component completed

--- src/Components/StudProfile.jsx
+++ src/Components/StudProfile.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import StudNavBar from './StudNavBar'
 
 const StudProfile = () => {
     const [studData, setStudData] = useState(
@@ -21,6 +22,7 @@ const StudProfile = () => {
 
     return (
         <div>
+            <StudNavBar/>
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
