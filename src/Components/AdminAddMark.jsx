2b9cc6463be91db998f698936b41541c31991f7c
Sree1710 <sreelekshmisl1710@gmail.com>
2023-11-05 18:11:18 +0530

    AdminNavbar component completed

--- src/Components/AdminAddMark.jsx
+++ src/Components/AdminAddMark.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import AdminNavbar from './AdminNavbar'
 
 const AdminAddMark = () => {
     const [inputField,setInputField]=useState(
@@ -27,6 +28,7 @@ const AdminAddMark = () => {
 
   return (
     <div>
+        <AdminNavbar/>
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
